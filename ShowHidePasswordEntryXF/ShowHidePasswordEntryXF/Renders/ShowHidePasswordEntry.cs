bd30cbebeaab344ed946e0610f4b53c40b35afde
Osvaldo Santiago <osvaldo-santiago@hotmail.com>
2017-01-25 22:12:30 -0600

    WIP - Show/hide password entry

---
+++ ShowHidePasswordEntryXF/ShowHidePasswordEntryXF/Renders/ShowHidePasswordEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ShowHidePasswordEntryXF
+{
+	public class ShowHidePasswordEntry:Entry
+	{
+		public ShowHidePasswordEntry()
+		{
+		}
+		public string EntryText { get; set; }
+	}
+}
---
+++ ShowHidePasswordEntryXF/ShowHidePasswordEntryXF/ShowHidePasswordEntryXFPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ShowHidePasswordEntryXF
+{
+	public partial class ShowHidePasswordEntryXFPage : ContentPage
+	{
+		public ShowHidePasswordEntryXFPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
